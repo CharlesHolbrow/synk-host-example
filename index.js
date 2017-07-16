ebc1fe38156d298cbf4020c3a5ae5b9b1c02bcf3
Charles Holbrow <charlesholbrow@gmail.com>
2017-07-16 00:02:20 -0400

    webpack

--- index.js
+++
@@ -1,4 +0,0 @@
-import TestClass from 'synk-js';
-
-const t = new TestClass();
-console.log('synk-host!!');
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { Conn } from 'synk-js';
+
+const t = new Conn();
+
+console.log('synk-host!!');
